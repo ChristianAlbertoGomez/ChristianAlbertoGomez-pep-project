d510dddde2b69382a2cfd9643d8c481b1e617a56
Christian Gomez <cgome39@gmail.com>
2024-08-13 19:46:57 +0000

    Services Classes Added.

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,5 +1,16 @@
 package Service;
+import DAO.AccountDAO;
+import Model.Account;
 
 public class AccountService {
-    
+
+    private final AccountDAO accountDAO = new AccountDAO();
+
+    public Account registerAccount(){
+        return null;
+    }
+
+    public Account loginAccount(){
+        return null;
+    }
 }
--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,5 +1,34 @@
 package Service;
+import DAO.MessageDAO;
+import Model.Message;
+import java.util.List;
 
 public class MessageService {
-    
+
+    private final MessageDAO messageDAO = new MessageDAO();
+
+    public Message createMessage(Message msg){
+        return null;
+    }
+
+    public List<Message> getAllMessages(){
+        return null;
+    }
+
+    public Message getMessageById(int msgId){
+        return null;
+    }
+
+    public Message deleteMessageById(Message msg){
+        return null;
+    }
+
+    public Message updateMessage(Message msg){
+        return null;
+    }
+
+    public List<Message> getMessagesByUserId(int accountId){
+        return null;
+    }
+
 }
