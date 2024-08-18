c555018fe2ec4a383a09a395b7248a70fa85ffc4
Christian Gomez <cgome39@gmail.com>
2024-08-18 17:30:59 +0000

    Just testing

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -8,33 +8,24 @@ import Model.Account;
 import Model.Message;
 import java.util.List;
 
-/**
- * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
- * found in readme.md as well as the test cases. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
 public class SocialMediaController {
 
     private final AccountService accountService = new AccountService();
     private final MessageService messageService = new MessageService();
+    private final int num = 0;
 
-    /**
-     * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
-     * suite must receive a Javalin object from this method.
-     * @return a Javalin app object which defines the behavior of the Javalin controller.
-     */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        app.get("/example-endpoint", this::exampleHandler);
 
         app.post("/register", this::registerUser);
-        app.post("login",this::loginUser);
+        app.post("/login", this::loginUser);
         app.post("/messages", this::createMessage);
         app.get("/messages", this::getAllMessages);
-        app.get("/messages/:message_id", this::getMessageById);
-        app.delete("/messages/:message_id", this::deleteMessageById);
-        app.patch("/messages/:message_id", this::updateMessageById);
-        app.get("/accounts/:account_id/messages", this::getMessagesByUserId);
+        app.get("/messages/{message_id}", this::getMessageById);
+        app.delete("/messages/{message_id}", this::deleteMessageById);
+        app.patch("/messages/{message_id}", this::updateMessageById);
+        app.get("/accounts/{account_id}/messages", this::getMessagesByUserId);
 
         return app;
     }
@@ -154,13 +145,7 @@ public class SocialMediaController {
         }
     }
 
-    /**
-     * This is an example handler for an example endpoint.
-     * @param context The Javalin Context object manages information about both the HTTP request and response.
-     */
     private void exampleHandler(Context context) {
         context.json("sample text");
     }
-
-
 }
